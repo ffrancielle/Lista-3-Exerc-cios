5c22efc235f1e9263faa3ad999c024485ba00f7c
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2024-07-01 20:14:19 -0300

    Add files via upload

---
+++ Lista3ex1/Lista3ex1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            /* do/while
+
+            do
+            {
+            
+            Console.Write("Digite um valor positivo: ");
+            a = int.Parse(Console.ReadLine());
+
+            }
+            while (a <= 0);
+            Console.WriteLine("O número digitado foi (0)", a);
+            */
+
+            a = 0;
+            while (a <= 0)
+            {
+                Console.WriteLine("Digite um valor positivo: ");
+                a = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("O número digitado foi {0}", a);
+
+
+            
+
+        }
+    }
+}
