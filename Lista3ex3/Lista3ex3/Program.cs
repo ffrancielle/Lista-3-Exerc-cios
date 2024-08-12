632bfa3e7dfe42143bc7fbac4ecdd0e234d3f645
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2024-08-12 15:37:12 -0300

    Add files via upload

---
+++ Lista3ex3/Lista3ex3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sexo;
+
+            do
+            {
+                Console.Write("Digite o sexo da pessoa (F para feminino, M para masculino): ");
+                sexo = Console.ReadLine().ToUpper();
+
+                if (sexo != "F" && sexo != "M")
+                {
+                    Console.WriteLine("Entrada inválida. Por favor, digite 'F' para feminino ou 'M' para masculino.");
+                }
+            } while (sexo != "F" && sexo != "M");
+
+            Console.WriteLine("Sexo válido registrado: {0}", sexo);
+
+        }
+    }
+}
