1c7ff4a6147aad6b38488939a57422bb8e0bef88
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2024-08-13 20:07:04 -0300

    Add files via upload

---
+++ Lista3ex4/Lista3ex4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3ex4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int numero = 5;
+            int i;
+
+
+            Console.WriteLine("Tabuada do número {0}:", numero);
+            for (i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} x {1} = {2}", numero, i, numero * i);
+            }
+        }
+    }
+}
