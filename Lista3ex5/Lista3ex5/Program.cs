d03385b08b6614356cff4baac64e9783fb1246fb
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2024-08-13 20:29:13 -0300

    Add files via upload

---
+++ Lista3ex5/Lista3ex5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3ex5
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int valor;
+            int i;
+
+            do
+            {
+                Console.Write("Digite um valor positivo: ");
+                valor = Convert.ToInt32(Console.ReadLine());
+
+                if (valor <= 0)
+                {
+                    Console.WriteLine("Valor inválido. Por favor, digite um número positivo.");
+                }
+            } while (valor <= 0);
+
+            Console.WriteLine("Tabuada do número {0}:", valor);
+            for (i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} x {1} = {2}", valor, i, valor * i);
+            }
+        }
+    }
+}
