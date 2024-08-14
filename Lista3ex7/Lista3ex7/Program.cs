cc05d58c4c0a96ed83467171407ab030f9ba000c
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2024-08-13 23:05:29 -0300

    Add files via upload

---
+++ Lista3ex7/Lista3ex7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3ex7
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int valor = 1;
+            int multiplicador;
+            int resultado;
+
+            while (valor <= 20)
+            {
+                Console.WriteLine("Tabuada do número {0}:", valor);
+
+                multiplicador = 1;
+                while (multiplicador <= 10)
+                {
+                    resultado = valor * multiplicador;
+                    Console.WriteLine("{0} x {1} = {2}", valor, multiplicador, resultado);
+                    multiplicador++;
+                }
+
+                Console.WriteLine("Pressione qualquer tecla para ver a próxima tabuada...");
+                Console.ReadKey();
+
+                valor++;
+            }
+
+            Console.WriteLine("Fim das tabuadas.");
+        }
+    
+    }
+}
