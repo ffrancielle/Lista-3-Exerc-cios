811f5c5c6dbe851e871a2be879c8199289cae592
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2024-08-13 23:11:46 -0300

    Add files via upload

---
+++ Lista3ex9/Lista3ex9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anterior = 0;
+            int atual = 1;
+            int proximo;
+            int contador = 1;
+
+            Console.WriteLine("Série de Fibonacci - 30 primeiros valores:");
+
+            while (contador <= 30)
+            {
+                Console.WriteLine("{0}: {1}", contador, atual);
+
+                proximo = anterior + atual;
+                anterior = atual;
+                atual = proximo;
+
+                contador++;
+            }
+        }
+    }
+}
