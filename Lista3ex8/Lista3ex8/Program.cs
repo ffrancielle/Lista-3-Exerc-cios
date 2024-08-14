4391b485279880e72c4937d87d15ca9dcf075288
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2024-08-13 23:08:23 -0300

    Add files via upload

---
+++ Lista3ex8/Lista3ex8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+            int numero = 1;
+
+            while (numero <= 100)
+            {
+                soma += numero;
+                numero++;
+            }
+
+            Console.WriteLine("A soma dos números inteiros de 1 a 100 é: {0}", soma);
+        }
+    }
+}
